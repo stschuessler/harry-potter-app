86da158d550813185e0e0b84885a6345624d44a8
Sarah Schüßler <stschuessler@googlemail.com>
2021-09-10 12:08:12 +0200

    Add fetch to function

--- main.js
+++ main.js
@@ -1,5 +1,12 @@
 function fetchExample() {
   console.log('Hallo Welt')
+
+  fetch('http://hp-api.herokuapp.com/api/characters')
+    .then((response) => response.json())
+    .then((allData) => {
+      console.log('Datanabruf hat funktioniert')
+      console.log(allData)
+    })
 }
 
 fetchExample()
