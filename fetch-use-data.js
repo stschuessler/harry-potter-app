dbe0578997c937511dc2a9f23dec917b0be56cf0
Sarah Schüßler <stschuessler@googlemail.com>
2021-09-12 19:36:54 +0200

    Add names and pictures

--- fetch-use-data.js
+++ fetch-use-data.js
@@ -1,15 +1,33 @@
+//const body = document.querySelector('body')
+//const box = document.createElement('div')
+// box.innerHTML = `Lasst mich euch Ron vorstellen
+//   <img src="http://hp-api.herokuapp.com/images/ron.jpg" alt="Picture of Ron">`
+// body.appendChild(box)
+
 export function doSomethingWithData(data) {
+  //console.table(data)
   const body = document.querySelector('body')
+  const nameArray = data.map((allNames) => allNames.name)
+  const imageArray = data.map((allImages) => allImages.image)
+
+  console.log(nameArray)
+  console.log(imageArray)
+
   const box = document.createElement('div')
-  box.innerHTML = `Lasst mich euch Ron vorstellen
-  <img src="http://hp-api.herokuapp.com/images/ron.jpg" alt="Picture of Ron">`
+
   body.appendChild(box)
 
-  console.log(data)
-  data.forEach((character) => {
-    console.table(character)
-    //   //console.table(character.image)
-    //   // console.log(character.name)
-    //   //console.log()
+  nameArray.forEach((element) => {
+    const paragraph = document.createElement('p')
+    paragraph.innerHTML = `Let me introduce you to ${element}`
+    box.appendChild(paragraph)
+  })
+
+  imageArray.forEach((element) => {
+    const images = document.createElement('img')
+    images.setAttribute('src', `${element}`)
+    images.setAttribute('width', '200')
+    images.setAttribute('alt', 'A picture of XX')
+    box.appendChild(images)
   })
 }
